d82ad2129448f03a580417ee54a0ec5acc5ef7f7
Eric Osterberg <ejosterberg@gmail.com>
2026-05-06 23:26:46 -0500

    18.0.0.1.4: settings layout polish + engine_version stamp on audit JSON
    Signed-off-by: Eric Osterberg <ejosterberg@gmail.com>

--- account_ostax/__manifest__.py
+++ account_ostax/__manifest__.py
@@ -5,7 +5,7 @@
         "Replace static US sales-tax rates with destination-based "
         "lookups against an OpenSalesTax engine instance."
     ),
-    "version": "18.0.0.1.3",
+    "version": "18.0.0.1.4",
     "category": "Accounting/Localizations",
     "website": "https://github.com/ejosterberg/opensalestax-odoo",
     "author": "Eric Osterberg",
