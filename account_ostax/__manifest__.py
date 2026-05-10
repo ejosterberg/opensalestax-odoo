cf5da3489c7a90285910c20d0b2f5d951071e526
Eric Osterberg <ejosterberg@gmail.com>
2026-05-10 00:11:39 -0500

    16.0.0.3.1: cherry-pick v0.3.1 PyPI docs refresh
    Signed-off-by: Eric Osterberg <ejosterberg@gmail.com>

--- account_ostax/__manifest__.py
+++ account_ostax/__manifest__.py
@@ -5,7 +5,7 @@
         "Replace static US sales-tax rates with destination-based "
         "lookups against an OpenSalesTax engine instance."
     ),
-    "version": "16.0.0.3.0",
+    "version": "16.0.0.3.1",
     "category": "Accounting/Localizations",
     "website": "https://github.com/ejosterberg/opensalestax-odoo",
     "author": "Eric Osterberg",
